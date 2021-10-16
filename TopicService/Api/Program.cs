e19cf6c8ed7a9a90bb2cf271d59d53a9d9bc6e09
Leeqviz <leeqviz@gmail.com>
2021-10-17 00:07:23 +0300

    code refactor

--- TopicService/Api/Program.cs
+++ TopicService/Api/Program.cs
@@ -13,7 +13,7 @@ namespace TopicService.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run(); // init
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
