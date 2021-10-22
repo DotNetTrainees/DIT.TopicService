0ac8d72b7c12b6ff8a1769c870feac7500640aa1
Leeqviz <leeqviz@gmail.com>
2021-10-22 19:45:47 +0300

    tests

---
+++ TopicService/Tests/TestServerFixture.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Net.Http;
+using TopicService.Api;
+
+namespace TopicService.Tests
+{
+    public class TestServerFixture
+    {
+        public TestServer TestServer { get; }
+        public HttpClient Client { get; }
+
+        public TestServerFixture()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var webBuilder = new WebHostBuilder()
+                .UseConfiguration(builder.Build())
+                .UseStartup<Startup>();
+
+            TestServer = new TestServer(webBuilder);
+            Client = TestServer.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            TestServer.Dispose();
+        }
+    }
+}
